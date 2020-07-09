9767c292d5ae63e3e6fe4de997739530a84f7407
CodeHero <imissyou5201314@outlook.com>
2020-07-09 16:21:07 +0800

    yml配置完毕

--- src/main/java/club/codehero/BlogApplication.java
+++ src/main/java/club/codehero/BlogApplication.java
@@ -1,4 +1,4 @@
-package club.codehero.blog;
+package club.codehero;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- src/test/java/club/codehero/BlogApplicationTests.java
+++ src/test/java/club/codehero/BlogApplicationTests.java
@@ -1,4 +1,4 @@
-package club.codehero.blog;
+package club.codehero;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
